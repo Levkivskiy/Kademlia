76ef0eb36709349d912ede57e04bef4e81c79024
Levkivskiy <olevkivskiy@gmail.com>
2018-12-16 01:24:09 +0200

    Init

--- src/main/scala/node/KademliaServer.scala
+++ src/main/scala/node/KademliaServer.scala
@@ -1,13 +1,18 @@
 package node
 
-import java.net.{InetAddress, InetSocketAddress}
+import java.net.InetSocketAddress
 
 import akka.actor.{ActorSystem, Props}
+import akka.io.Udp
+import akka.pattern.ask
+import akka.util.Timeout
 import ident.KadId
 import messeges.ServerEvents.{Bootstrap, SendMsg}
-import network.Connect
+import network.{Connect, StartCheck, StartConnection}
 
-import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContextExecutor}
+import util.GlobalConfig
 
 @SerialVersionUID(10L)
 case class NodeInform(inetSocketAddress: InetSocketAddress, kadId: KadId)
@@ -25,9 +30,32 @@ class KademliaServer(kadId: KadId, hostName: String, port: Int) {
   val node = system.actorOf(Props(new Node(nodeInform)))
 
   def bootstrap(nodeInf: NodeInform) = {
-    val connection = system.actorOf(Props(new Connect(nodeInform)))
+    val connection = newConnection(nodeInf)
+    connection ! StartConnection
+//    //todo move start to newConn def
+//    def _startChecker(attemptsLeft: Int, timeBreak: Int): Boolean = {
+//      implicit val timeout: Timeout = 10.second
+//      val res = Await.result(ask(connection, StartCheck).mapTo[Boolean], 10000.second)
+//      res match {
+//        case true => true
+//        case false => attemptsLeft match {
+//          case 0 => false
+//          case left =>
+//            Thread.sleep(timeBreak * 60)
+//            _startChecker(left - 1, timeBreak)
+//        }
+//      }
+//      res
+//    }
+//    _startChecker(GlobalConfig.attemptsToConnect, GlobalConfig.timeBreakTiConnectSeconds) match {
+//      case true => connection ! SendMsg(Bootstrap(nodeInform))
+//      case _ =>
+//    }
+    Thread.sleep(1000)
     connection ! SendMsg(Bootstrap(nodeInform))
+    //connection ! Udp.Unbind
   }
 
+  def newConnection(nodeInf: NodeInform) = system.actorOf(Props(new Connect(nodeInf)))
   def getNodeInforn = nodeInform
 }
---
+++ src/main/scala/util/GlobalConfig.scala
@@ -0,0 +1,6 @@
+package util
+
+object GlobalConfig {
+  val attemptsToConnect = 10
+  val timeBreakTiConnectSeconds = 2
+}
