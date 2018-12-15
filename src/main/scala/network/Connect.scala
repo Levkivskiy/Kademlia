14136512cd7dd3c6f1316d72bda36a4ecfe0e5b2
Levkivskiy <olevkivskiy@gmail.com>
2018-12-16 01:24:09 +0200

    Init

--- src/main/scala/network/Connect.scala
+++ src/main/scala/network/Connect.scala
@@ -7,25 +7,32 @@ import messeges.ServerEvents._
 import node.NodeInform
 import util.Serialize
 
+//todo move to mess pack
+case object StartConnection
+case object StartCheck
+
 class Connect(nodeInform: NodeInform) extends Actor with ActorLogging {
   import context.system
 
-  override def preStart(): Unit = {
-    IO(UdpConnected) ! UdpConnected.Connect(self, nodeInform.inetSocketAddress)
-  }
+  var isStartConnect = false
 
   def receive = {
+    case StartConnection => IO(UdpConnected) ! UdpConnected.Connect(self, nodeInform.inetSocketAddress)
+
     case UdpConnected.Connected => {
+      isStartConnect = true
       log.info(s"Conect operation to ${nodeInform.inetSocketAddress}")
       context.become(ready(sender()))
     }
   }
   def ready(connection: ActorRef):Receive = {
+    case StartCheck => sender() ! isStartConnect
+
     case UdpConnected.Received(data) =>
       log.info(s"Socket ${nodeInform.inetSocketAddress} receive data " +
         s"${Serialize.deserialize(data.toArray)}")
     case SendMsg(msg) ⇒
-      connection ! UdpConnected.Send(ByteString())
+      connection ! UdpConnected.Send(ByteString(Serialize.serialize(msg)))
     case UdpConnected.Disconnect =>
       connection ! UdpConnected.Disconnect
     case UdpConnected.Disconnected => context.stop(self)
