5caf8789b1a70b0ed0a00ef40245aa9937f70501
Levkivskyi <olevkivskiy@gmail.com>
2018-12-29 16:06:05 +0200

    Make connection

--- src/main/scala/node/KademliaServer.scala
+++ src/main/scala/node/KademliaServer.scala
@@ -2,16 +2,20 @@ package node
 
 import java.net.InetSocketAddress
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.io.{Udp, UdpConnected}
+import akka.japi.Option.Some
 import akka.pattern.ask
 import akka.util.Timeout
 import ident.KadId
 import messeges.ServerEvents.{Bootstrap, SendMsg}
-import network.{Conn, Connect}
+import network.{Connect, ConnectHelper}
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContextExecutor}
-import util.GlobalConfig
+import util.GlobalConfig._
+import messeges.SocketEvents
+import messeges.SocketEvents._
 
 @SerialVersionUID(10L)
 case class NodeInform(inetSocketAddress: InetSocketAddress, kadId: KadId)
@@ -29,12 +33,29 @@ class KademliaServer(kadId: KadId, hostName: String, port: Int) {
   val node = system.actorOf(Props(new Node(nodeInform)))
 
   def bootstrap(nodeInf: NodeInform) = {
-    val connection = newConnection(nodeInf)
-    connection ! Conn
-    Thread.sleep(1000)
-    connection ! SendMsg(Bootstrap(nodeInform))
+    newConnection(nodeInf).get match {
+      case connection: ActorRef => {
+        connection ! SendMsg(Bootstrap(nodeInf))
+
+        connection ! UdpConnected.Disconnect
+      }
+      case _ =>
+    }
   }
 
-  def newConnection(nodeInf: NodeInform) = system.actorOf(Props(new Connect(nodeInf)))
+  def newConnection(nodeInf: NodeInform): Option[ActorRef] = {
+    val connListener = new ConnectHelper()
+    val connection = system.actorOf(Props(new Connect(nodeInf, connListener)))
+
+    connection ! StartConnect
+    val waitTime = ((attemptsToConnect * timeBreakToConnectSeconds) + 5).second
+    implicit val timeout: Timeout = waitTime
+
+    connListener.startWaitConn() match {
+      case true => Some(connection)
+      case false => None
+    }
+  }
+
   def getNodeInforn = nodeInform
 }
