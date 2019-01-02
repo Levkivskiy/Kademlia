412ca393db18bb34ff7ac5b37540b94aede3c96c
Levkivskyi <olevkivskiy@gmail.com>
2019-01-02 23:32:26 +0200

    kbucketStorage tested

--- src/test/scala/kbucketTest/KBucketTestSpec.scala
+++ src/test/scala/kbucketTest/KBucketTestSpec.scala
@@ -2,18 +2,20 @@ package kbucketTest
 
 import java.net.InetAddress
 
-import akka.actor.ActorRef
+import akka.actor.{ActorRef, ActorSystem}
 import ident.KadId
 import org.scalatest.FlatSpec
-import kbucket.KBucket
+import kbucket.{KBucket, KBucketsContainers}
+import network.Connect
 import node.KademliaServer
 
 class KBucketTestSpec extends FlatSpec {
-  val node = new KademliaServer(KadId(), InetAddress.getLocalHost.getHostAddress, 0)
-  val node1 = new KademliaServer(KadId(), InetAddress.getLocalHost.getHostAddress, 0)
-  val node2 = new KademliaServer(KadId(), InetAddress.getLocalHost.getHostAddress, 0)
-  val node3 = new KademliaServer(KadId(), InetAddress.getLocalHost.getHostAddress, 0)
-  val node4 = new KademliaServer(KadId(), InetAddress.getLocalHost.getHostAddress, 0)
+
+  val node = new KademliaServer(KadId("asd13e12"), InetAddress.getLocalHost.getHostAddress, 61212)
+  val node1 = new KademliaServer(KadId(), InetAddress.getLocalHost.getHostAddress, 61322)
+  val node2 = new KademliaServer(KadId(), InetAddress.getLocalHost.getHostAddress, 60123)
+  val node3 = new KademliaServer(KadId(), InetAddress.getLocalHost.getHostAddress, 60975)
+  val node4 = new KademliaServer(KadId(), InetAddress.getLocalHost.getHostAddress, 61145)
 
   val connection1 = node.newConnection(node1.nodeInform).get
   val connection2 = node.newConnection(node2.nodeInform).get
@@ -41,4 +43,24 @@ class KBucketTestSpec extends FlatSpec {
     kBucket.remove(connection3)
   }
 
+  val kBucketsStorage = new KBucketsContainers(node.nodeInform.kadId)
+
+  "A set of kbuckets" should "Add" in {
+    kBucketsStorage.add(connection1)
+    kBucketsStorage.add(connection2)
+    kBucketsStorage.add(connection3)
+
+    assert(kBucketsStorage.contains(connection1))
+    assert(kBucketsStorage.contains(connection2))
+    assert(kBucketsStorage.contains(connection3))
+
+    assert(!kBucketsStorage.contains(connection4))
+  }
+  it should "remove" in {
+    assert(kBucketsStorage.remove(connection1))
+    assert(kBucketsStorage.remove(connection2))
+    assert(kBucketsStorage.remove(connection3))
+
+    assert(!kBucketsStorage.remove(connection1))
+  }
 }
